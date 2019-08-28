aa4066ea7acdbf48935c83e5895c7f8f194f0ab8
Hamza <Hamza@hamzas-mbp.home>
2019-08-28 13:49:39 +0100

    Finished Blog

---
+++ index.js
@@ -0,0 +1,31 @@
+function button1Click(){
+    window.location='London.html'
+}
+
+function button2Click(){
+    window.location='Portsmouth.html'
+}
+
+function button3Click(){
+    window.location='AlAin.html'
+}
+
+function AboutButtonClick(){
+    window.location="about.html"
+}
+
+function HomeButtonClick(){
+    window.location="index.html"
+}
+
+function LondonButtonClick(){
+    window.location="London.html"
+}
+
+function PortsmouthButtonClick(){
+    window.location="Portsmouth.html"
+}
+
+function AlAinButtonClick(){
+    window.location="AlAin.html"
+}
